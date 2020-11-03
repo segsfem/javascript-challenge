b67483c91df7b93959c5201fbf6347942e5d1c5c
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-11-03 11:40:37 -0500

    main

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,7 +5,7 @@ var tableData = data;
 // Select button
 var button = d3.select("#filter-btn");
 // Select form
-var form = d3.select("#datetime");
+var form = d3.select("#ufo-table");
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 console.log(data)
@@ -19,28 +19,45 @@ function runEnter() {
     d3.event.preventDefault();
 
     //Select input elements
-    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#ufo-table");
     var inputValue = inputElement.property("value");
 
     console.log(inputValue);
     console.log(tableData);
 
+data.forEach( ufo => {
+    let tr =  tbody.append("tr");
+    Object.entries(ufo).forEach( function([key,value]) {
+       let cell=tr.append("td");
+       cell.text(value);
+    }); 
+    
+});
+
     var filteredDate = tableData.filter(ufo => ufo.datetime === inputValue);
-    var filteredCity = tableData.filter(ufo => ufo.city === inputValue);
-    var filteredState = tableData.filter(ufo => ufo.state === inputValue);
-    var filteredCountry = tableData.filter(ufo => ufo.country === inputValue);
-    var filteredShape = tableData.filter(ufo => ufo.state === inputValue);
-    var filteredDuration = tableData.filter(ufo => ufo.durationMinutes === inputValue);
-    var filteredComments = tableData.filter(ufo => ufo.comments === inputValue);
+    // var filteredCity = tableData.filter(ufo => ufo.city === inputValue);
+    // var filteredState = tableData.filter(ufo => ufo.state === inputValue);
+    // var filteredCountry = tableData.filter(ufo => ufo.country === inputValue);
+    // var filteredShape = tableData.filter(ufo => ufo.state === inputValue);
+    // var filteredDuration = tableData.filter(ufo => ufo.durationMinutes === inputValue);
+    // var filteredComments = tableData.filter(ufo => ufo.comments === inputValue);
 
     console.log(filteredDate);
-    console.log(filteredCity);
+    // console.log(filteredCity);
 
 }
 
 //remove any children from the list
-List.html("");
+// List.html("");
 
-//append filter to list
-tbody.append("td").text(`${filteredDate}`);
-list.append("li").text(`${filteredDate}`);
+//append filter to table
+
+// tbody.append("tr").text(`${filteredDate[0]}`);
+// tbody.append("tr").text(`${filteredDate[1]}`);
+// tbody.append("tr").text(`${filteredDate[2]}`);
+// tbody.append("tr").text(`${filteredDate[3]}`);
+// tbody.append("tr").text(`${filteredDate[4]}`);
+// tbody.append("tr").text(`${filteredDate[5]}`);
+// tbody.append("tr").text(`${filteredDate[6]}`);
+// list.append("li").text(`${filteredDate}`);
+
